0808093b90a43ffb35d7a4ce553f68ff5dbb7e4a
jiandeng <jiandeng.develop@gmail.com>
2020-02-11 11:51:18 +0800

    Silly support for gcc compiler

---
+++ module/app_dummy_display.c
@@ -0,0 +1,19 @@
+#include "app_display.h"
+
+void app_display_init(app_display_content_t *initial_state)
+{
+}
+
+void app_display_create_main_screen(app_display_content_t *content)
+{
+}
+
+
+
+void app_display_update_main_screen(app_display_content_t *content)
+{
+}
+
+void app_display_update(void)
+{
+}
